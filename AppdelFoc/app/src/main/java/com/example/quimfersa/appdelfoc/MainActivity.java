1f55352c15b45b93cda79ccf2b5065098b8c078d
ferrerax <quimfersa@hotmail.com>
2019-02-16 00:11:13 +0100

    gumersindo

--- AppdelFoc/app/src/main/java/com/example/quimfersa/appdelfoc/MainActivity.java
+++ AppdelFoc/app/src/main/java/com/example/quimfersa/appdelfoc/MainActivity.java
@@ -149,6 +149,8 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             s = new Socket(IP, PORT); //connect to server at port port
+            s.setSoTimeout(30000);
+            s.setKeepAlive(true);
             pw = new PrintWriter(s.getOutputStream());
             pw.write(resultat);
 
